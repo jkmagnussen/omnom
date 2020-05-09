31f93a6dd61295900aaec4093256a1d14b422bc7
Joseph Karl Magnussen <jkmagnussen@outlook.com>
2020-05-09 15:25:33 +0100

    + sprtByOption state set & ClassName highlight on active choice

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,25 +1,45 @@
 import React from "react";
 import "./SearchBar.css";
 
-const sortByOptions = {
-    'Best Match': 'best_match',
-    'Highest Rated': 'rating',
-    'Most Reviewed': 'review_count'
-  }
 
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props)
 
+    this.state = {term:'', location:'', sortBy: 'rating'}
+    this.sortByOptions = {
+      'Best Match': 'best_match',
+      'Highest Rated': 'rating',
+      'Most Reviewed': 'review_count'
+    }
+  }
 
     renderSortByOptions() {
-      return Object.keys(sortByOptions).map(sortByOption => 
+      return Object.keys(this.sortByOptions).map(sortByOption => 
         {
-        let sortByOptionValue = sortByOptions[sortByOption]
-        return (<li key={sortByOptionValue}>{sortByOption}</li>);
+        let sortByOptionValue = this.sortByOptions[sortByOption]
+        return (<li 
+          className={this.getSortByClass(sortByOptionValue)}
+        key={sortByOptionValue}
+        onClick={this.handleSortByChange.bind(this, sortByOptionValue)}
+        >
+          {sortByOption}
+        </li>);
       })
     }
+    getSortByClass (sortByOption) {
+      if(this.state.sortBy === sortByOption){
+        return 'active'
+      } else {
+        return ''
+      }
+    }
+    handleSortByChange(sortByOption){
+      this.setState({
+        sortBy: sortByOption
+      });
+    }
     
-
-
   render() {
     return (
       <div className="SearchBar">
